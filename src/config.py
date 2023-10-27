ff19b40a08b13217367b64621a3afaa5c44aa6cb
Jathavaan Shankarr <jathavaan12@gmail.com>
2023-10-27 14:22:24 +0200

    Initial project structure

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+from enum import Enum
+
+
+class Config(Enum):
+    # Paths
+    ROOT_DIR: str = os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__)))))
+    DATA_DIR: str = os.path.join(ROOT_DIR, 'data')
+    INPUT_DIR: str = os.path.join(DATA_DIR, 'input')
+    RAW_DATA_DIR: str = os.path.join(INPUT_DIR, 'raw_data')
+    CROPPED_DATA_DIR: str = os.path.join(INPUT_DIR, 'cropped_data')
+    OUTPUT_DIR: str = os.path.join(DATA_DIR, 'output')
